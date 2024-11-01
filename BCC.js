95f10006bbc3c9cf41071faf1e78277d86c50e28
terrydavisghost <134529210+terrydavisghost@users.noreply.github.com>
2024-11-01 23:03:49 +0800

    Add files via upload

---
+++ BCC.js
@@ -0,0 +1,35 @@
+ //window.onscroll = function() {scrollFunction()};
+
+ //function scrollFunction() {
+    //the scroll function evokes the code when the condition in the brackets is met
+   //if (document.body.scrollTop > 80 ||
+    //the if keyword executes the code when the screen has been scrolled down by 80 pixels
+    // document.documentElement.scrollTop > 80) {
+      //these are the code that will get executed and 
+    // {
+     //document.getElementsByTagName('nav')
+     //    .style.height = "220px"; 
+     //document.getElementById("nav-logo")
+      //   .style.height = "100.45px";
+    // document.getElementById("nav-logo")
+        // .style.width = "269.5px";
+     //document.getElementById("nav-logo-condensed")
+     //    .style.width = "80px"; 
+     //document.getElementById("nav-logo-condensed")
+     //    .style.height = "80px"; 
+   //}
+   //} else 
+   //the "else" operator means these will be the styles when the condition above is not met
+   //{
+   //  document.getElementsByTagName('nav')
+    //     .style.height = "269.5px"; 
+    // document.getElementById("nav-logo")
+    //     .style.height = "100.45px";
+    // document.getElementById("nav-logo")
+    //     .style.width = "269.5px";
+    // document.getElementById("nav-logo-condensed")
+    //     .style.width = "100px"; 
+    // document.getElementById("nav-logo-condensed")
+   //      .style.height = "100px"; 
+  // }
+ //} 
